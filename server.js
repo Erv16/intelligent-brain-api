1343b78044932be11f7a1b65b7094fe796ce8c2d
Erv16 <epalani1@binghamton.edu>
2020-01-20 18:27:05 +0530

    Added routing for profile and image

--- server.js
+++ server.js
@@ -30,6 +30,20 @@ app.get('/', (req, res) => {
   res.send(database.users);
 });
 
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(404).json('no such user');
+  }
+});
+
 app.post('/signin', (req, res) => {
   if (
     req.body.email === database.users[0].email &&
@@ -52,6 +66,21 @@ app.post('/register', (req, res) => {
     joined: new Date()
   });
   res.json(database.users[database.users.length - 1]);
+});
+
+app.post('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(404).json('not found');
+  }
 });
 
 app.listen(3000, () => {
