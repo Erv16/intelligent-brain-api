6c2d313032258d9e7b8512508fe7e24e76526152
Erv16 <epalani1@binghamton.edu>
2020-01-22 17:04:01 +0530

    Created a new end point for interfacing with the clarifai api and moved this interfacing functionality to the backend to prevent end users from accessing the api key

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '2b597a4debdc4994a19f6714297fb0e2'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
@@ -13,5 +28,6 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-  handleImage: handleImage
+  handleImage,
+  handleApiCall
 };
